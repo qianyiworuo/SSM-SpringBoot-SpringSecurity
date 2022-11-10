d5847e83ce2b33bc920b509b1069799a20485edc
niko <wangdiwrg@outlook.com>
2022-11-10 16:57:45 +0800

    代码生成器生成User相关代码

--- service-system/src/main/java/com/qianyi/system/controller/SysUserController.java
+++ service-system/src/main/java/com/qianyi/system/controller/SysUserController.java
@@ -1,6 +1,7 @@
 package com.qianyi.system.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author qianyi
  * @since 2022-11-10
  */
+@Api(tags = "用户管理接口")
 @RestController
 @RequestMapping("/admin/system/sysUser")
 public class SysUserController {
---
+++ service-system/src/main/java/com/qianyi/system/service/impl/SysUserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.qianyi.system.service.impl;
+
+import com.qianyi.model.system.SysUser;
+import com.qianyi.system.mapper.SysUserMapper;
+import com.qianyi.system.service.SysUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ *
+ * @author qianyi
+ * @since 2022-11-10
+ */
+@Service
+public class SysUserServiceImpl extends ServiceImpl<SysUserMapper, SysUser> implements SysUserService {
+
+}
