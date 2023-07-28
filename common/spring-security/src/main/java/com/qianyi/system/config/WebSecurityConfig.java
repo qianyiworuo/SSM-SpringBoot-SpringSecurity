60adb48a0a0985fccc0c283b1935d518f8ff166f
niko <wangdiwrg@outlook.com>
2023-07-28 14:40:10 +0800

    spring-security：自定义密码组件、自定义用户类、根据用户名查询用户信息。

--- common/spring-security/src/main/java/com/qianyi/system/config/WebSecurityConfig.java
+++ common/spring-security/src/main/java/com/qianyi/system/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.qianyi.system.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @Configuration
 @EnableWebSecurity//是否开启SpringSecurity的默认行为
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
